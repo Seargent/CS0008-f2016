95ed4eea79c37bb6dc32de31eb0067937565ef0e
David <Seargent>
2016-09-08 12:38:32 -0400

    Starting with Python, Chapter 2, Exercise 3

---
+++ lecture-20160901/Ch.2,Ex.3.py
@@ -0,0 +1,15 @@
+#
+# Template for code submission
+#
+# name  :Davyd
+# email :dab254@pitt.edu
+# date  :08.09.16
+# class :CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Starting with Python, Chapter 2, Exercise 3
+#
+n = float(input())
+x = float((n)/(4046.8564224))
+print(x)
